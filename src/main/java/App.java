6171f76fe088d7880fb5f7d91e5e0222adc67230
Shen <karth-shen@hotmail.com>
2019-05-20 13:58:41 -0400

    Google auth set up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
+import googledrive.DriveQuickStart;
 import motiondetector.SimpleMotionDetector;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class App {
-    public static void main (String []args){
+    public static void main (String []args) throws IOException, GeneralSecurityException {
+
+        DriveQuickStart driveStart = new DriveQuickStart();
+        driveStart.QuickStart(args);
+
         SimpleMotionDetector detector = new SimpleMotionDetector();
         while(detector.isRunning()){
 
